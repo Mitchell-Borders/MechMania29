ea81e70d64b599d99b7ce699abca948987bd3200
Gunnar Moody <moodygman04@gmail.com>
2023-09-23 17:08:47 -0500

    Update choose_strategy.py

--- strategy/choose_strategy.py
+++ strategy/choose_strategy.py
@@ -2,7 +2,6 @@ from strategy.random_strategy import RandomStrategy
 from strategy.simple_human_strategy import SimpleHumanStrategy
 from strategy.simple_zombie_mod import SimpleZombieModification
 from strategy.simple_zombie_strategy import SimpleZombieStrategy
-from strategy.base_zombie_strategy import BaseZombieStrategy
 from strategy.base_human_strategy import BaseHumanStrategy
 from strategy.stunlock_human_strategy import StunlockHumanStrategy
 from strategy.strategy import Strategy
