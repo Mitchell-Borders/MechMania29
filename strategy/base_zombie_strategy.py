b87f296332a13e214f7af3515b257ed8cb52b1dd
Mason Myles <mason.myles98@gmail.com>
2023-09-23 01:09:20 -0500

    zombie attack

--- strategy/base_zombie_strategy.py
+++ strategy/base_zombie_strategy.py
@@ -63,54 +63,89 @@ class BaseZombieStrategy(Strategy):
             ) -> list[AttackAction]:
 
         choices = [] # our attack
-
+        # Iterates over a single zombie's possible attacks
+        #NEED TO DO:
+        # - only assign one append to the choices array in here
         for [character_id, attacks] in possible_attacks.items():
             if len(attacks) == 0:
                 continue
+            
+            # humans = []  # holds humans that are in range
+            
+            
+            # # Gather list of humans in range
+            # for a in attacks:
+            #     if a.type is AttackActionType.CHARACTER:
+            #         humans.append(a)
+            #         human = game_state.characters[a.attacking_id]
 
-            humans = []  # holds humans that are in range
+            # for a in attacks:
+            #     if a.attacking_id == human.id:
+            #         choices.append(a)
 
-            # Gather list of humans in range
-            for a in attacks:
-                if a.type is AttackActionType.CHARACTER:
-                    humans.append(a)
-
-            # Initialize a list of what we can attack
-            attackList = []
+            # # Initialize a list of what we can attack
+            # attackList : game_state.characters.values() = []
 
             # Generate hitlist
             hitlist = self.hitlist()
 
+            humans = []
+            for a in attacks:
+                if a.type is AttackActionType.CHARACTER:
+                    
+                    human = game_state.characters[a.attacking_id]
+                    humans.append(human)
+            if not len(humans) == 0: # // attack a target with regards to class and hitpoint
+                 for x in humans:
+                      for h in hitlist:
+                           if x.class_type == h:
+                                if x.health == 1:
+                                     for a in attacks:
+                                         if a.attacking_id == x.id:
+                                            choices.append(a)
+                                     break
+                 if len(choices) == 0: # // attack a target indiscrimate of hit points
+                    for x in humans:
+                        for h in hitlist:
+                            if x.class_type == h:
+                                 for a in attacks:
+                                      if a.attacking_id == x.id:
+                                           choices.append(a)
+                                 break
+            elif(len(choices) == 0):  # The targets in range must be terrain. May as well attack one.
+                choices.append(random.choice(attacks))
             
-            # Iterate and get all characters in game_state
-            for i in game_state.characters.values():
+            
+            # # Iterate and get all characters in game_state
+            # for i in game_state.characters.values():
                 
-                # For all in chev distance, append it to the attackList
-                for x in range(-1, 2):
-                     for y in range(-1, 2):
-                        attackList.append(self.find_attack(i, x, y, game_state, character_id, attackList))
+            #     # For all in chev distance, append it to the attackList
+            #     for x in range(-1, 2):
+            #          for y in range(-1, 2):
+            #             self.find_attack(i, x, y, game_state, character_id, attackList)
 
             # Choice who to attack based on health and class type, prioritizing people with low hitpoints, then
             # Tracuers, Medics, Marksmen, etc.
-            if not len(humans) == 0: # // attack a target with regards to class and hitpoint
-                 for x in humans:
-                      for hitlist_index, h in enumerate(hitlist):
-                           if x.class_type == hitlist[hitlist_index]:
-                                if x.health == 1:
-                                     choices.append(AttackAction(character_id, x.id))
-                                     break
-                 if len(choices) == 0: # // attack a target indiscrimate of hit points
-                    for x in attackList:
-                        for hitlist_index, h in enumerate(hitlist):
-                            if x.class_type == hitlist[hitlist_index]:
-                                 choices.append(AttackAction(character_id, x.id))
-            elif(len(choices) == 0):  # The targets in range must be terrain. May as well attack one.
-                choices.append(random.choice(attacks))
-        print("____________________________________________________________________________")
-        print("Choices: " + choices)
-        print("Humans: " + humans)
-        print("AttackList: " + attackList)
-        print('End of decide attacks')
+            # if not len(attackList) == 0: # // attack a target with regards to class and hitpoint
+            #      for x in attackList:
+            #           for h in hitlist:
+            #                if x.class_type == h:
+            #                     if x.health == 1:
+            #                          choices.append(AttackAction(character_id, x.id))
+            #                          break
+            #      if len(choices) == 0: # // attack a target indiscrimate of hit points
+            #         for x in attackList:
+            #             for h in hitlist:
+            #                 if x.class_type == h:
+            #                      choices.append(AttackAction(character_id, x.id))
+            #                      break
+            # elif(len(choices) == 0):  # The targets in range must be terrain. May as well attack one.
+            #     choices.append(random.choice(attacks))
+        # print("____________________________________________________________________________")
+        # print("Choices: " + choices)
+        # print("Humans: " + humans)
+        # print("AttackList: " + attackList)
+        # print('End of decide attacks')
         return choices
     
     # Calculate manhattan_distance for movement
@@ -120,12 +155,11 @@ class BaseZombieStrategy(Strategy):
         return distance
     
     # Append any character that is not a zombie and is in range for attack to the list
-    def find_attack(self, target, x, y, game_state, character_id, attackList) -> list:
+    def find_attack(self, target, x, y, game_state, character_id, attackList):
         target.position.x = target.position.x + x
         target.position.y = target.position.y + y
-        if (target.position) == game_state.characters[character_id].position and not target.is_zombie: 
+        if target.position.x == game_state.characters[character_id].position.x and target.position.y == game_state.characters[character_id].position.y and not target.is_zombie: 
             attackList.append(target)
-        return attackList
     
     # Create priority list for who to attack first
     def hitlist(self) -> list:
